965c7968be4d22530a47ba2a9cac7d0125d8450c
Omkar Pradhan <42708220+omkarpradhan12@users.noreply.github.com>
2025-05-19 14:37:03 +0530

    Update data_util.py
    grouping changes

--- data_util.py
+++ data_util.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def get_cat_df(df):
-    cat_grp = df[['category','amount']].groupby(by='category').sum().reset_index()
+    cat_grp = df[['paid_by','amount']].groupby(by='paid_by').sum().reset_index()
     return cat_grp
 
